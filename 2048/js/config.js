687a12880fa3893bfe9497b13c40895e24bb6f4b
realdefinity <coltona766@gmail.com>
2026-02-10 19:19:52 -0800

    yeyeye

--- 2048/js/config.js
+++ 2048/js/config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
     size: 4,
-    animSpeed: 200, // Faster, snappier movement
+    animSpeed: 300, // Slower, fluid movement for that "liquid" feel
     gap: 12,
     maxFlux: 100,
     costs: { undo: 25, destroy: 60 }
