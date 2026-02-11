fce1d08e0eaa3b919d4c36aef88d04d9c1ed15cb
realdefinity <coltona766@gmail.com>
2026-02-10 19:16:56 -0800

    yes

--- 2048/js/config.js
+++ 2048/js/config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
     size: 4,
-    animSpeed: 250, // Reduced slightly to match the new "snap" elastic transition
+    animSpeed: 200, // Faster, snappier movement
     gap: 12,
     maxFlux: 100,
     costs: { undo: 25, destroy: 60 }
